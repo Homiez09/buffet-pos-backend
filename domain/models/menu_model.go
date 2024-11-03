bc026aaf1d1211e5dd63de1602f72e62284e288a
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-11-03 14:34:13 +0700

    feat(schema): update Description and ImageURL to nullable field

--- domain/models/menu_model.go
+++ domain/models/menu_model.go
@@ -5,9 +5,9 @@ import "github.com/google/uuid"
 type Menu struct {
 	ID          uuid.UUID   `json:"id" gorm:"primaryKey;type:uuid"`
 	Name        string      `json:"name" gorm:"type:varchar(255)"`
-	Description string      `json:"description" gorm:"type:text"`
+	Description *string     `json:"description" gorm:"type:text"`
 	CategoryID  *uuid.UUID  `json:"categoryId" gorm:"type:uuid"`
-	ImageURL    string      `json:"imageUrl" gorm:"type:varchar(255)"`
+	ImageURL    *string     `json:"imageUrl" gorm:"type:varchar(255)"`
 	IsAvailable bool        `json:"isAvailable" gorm:"type:boolean"`
 	Price       float64     `json:"price" gorm:"type:decimal"`
 	OrderItems  []OrderItem `json:"orderItems" gorm:"foreignKey:MenuID"`
